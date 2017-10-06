8ed3f2692e70158fa4feb9204731fd4d99c02ff1
Szymon Ramczykowski <s.ramczykowski@kainos.com>
2017-10-06 19:29:06 +0200

    Added  changes getItemIdByName method. Cosmetic changes.

--- src/test/java/workshops/exercises/GetRequests.java
+++ src/test/java/workshops/exercises/GetRequests.java
@@ -1,9 +1,7 @@
 package workshops.exercises;
 
 import com.jayway.restassured.response.Response;
-import com.jayway.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
-import sun.security.krb5.internal.AuthorizationDataEntry;
 
 import static com.jayway.restassured.RestAssured.given;
 
@@ -17,33 +15,29 @@ public class GetRequests {
 
 
 	public void displayBoards() {
-		//ToDo: add authenticatcion, add endpoint change this method to return Response type
-		 		given()
-				.when().get("");
+		 		 given()
+						.when()
+						.get("");
 
 	}
 
 	@Test
 	public void displayBoardDetailsTest() {
 		//ToDo: call displayBoards with prettyPrint() method here
+		displayBoards();
+	}
 
+
+	public String getBoardId(String boardName) {
+		Response respone = null;
+		return getItemIdByName(respone, boardName);
 	}
 
 	@Test
 	public void displayBoardIdTest() {
-		System.out.println(getBoardId(""));
+		System.out.println(getBoardId("nameOfYourBoard"));
 	}
 
-	public String getBoardId(String boardName) {
-
-		//ToDo:  Use displayBoard method to initiate response variable. Replace variable1 and variable2 with proper node names.
-		Response response = null;
-		int i = 0;
-		while (!response.jsonPath().getString("variable1[" + String.valueOf(i) + "]").equals(boardName)) {
-			i++;
-		}
-		return response.jsonPath().getString("variable2[" + String.valueOf(i) + "]");
-	}
 
 	/**
 	 * Exercise 4 Cards
@@ -60,19 +54,13 @@ public class GetRequests {
 
 	public String getCardId(String cardName, String boardName) {
 		//ToDo:  Use proper method to initiate response variable. Replace variable1 and variable2 with proper node names.
-		Response response = null;
-		int i = 0;
-		while (!response.jsonPath().getString("variable1[" + String.valueOf(i) + "]").equals(cardName)) {
-			i++;
-		}
-		return response.jsonPath().getString("variable2[" + String.valueOf(i) + "]");
+		return "";
 	}
 
 	@Test
 	public void displayCards() {
 		//ToDo: test getCards and getCardId methods
 	}
-
 
 	/**Exercise 5 Lists
 	 * Display list
@@ -81,4 +69,13 @@ public class GetRequests {
 
 	//ToDo: Create getList(), getListId() methods with proper parameters. Use endpoint: "/boards/"+getBoardId(boardName)+"/lists"
 	//Hint: You might need 2 parameters in getListId method
+
+
+	public static String getItemIdByName(Response response, String itemName) {
+		int i = 0;
+		while (!response.jsonPath().getString("name[" + String.valueOf(i) + "]").equals(itemName)) {
+			i++;
+		}
+		return response.jsonPath().getString("id[" + String.valueOf(i) + "]");
+	}
 }
